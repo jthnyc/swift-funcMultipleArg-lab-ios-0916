522e815c271e2263029aa5b1644c90d326b523fa
Joanna Tzu-Hsuan Huang <joannathhuang@Joannas-MacBook-Pro.local>
2016-08-30 15:26:41 -0400

    Done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,7 +13,11 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func continentInfo(name: String, number: Int){
+    print("\(name) is a continent which contains \(number) countries")
+}
+
+continentInfo("Africa", number: 50)
 
 
 
@@ -21,11 +25,12 @@
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
+// the code below doesn't work, because the second argument type needs to be specified. Such as greeting("Danny", greeting: "Hello")
 func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting("Danny", "Hello")
+greeting("Danny", greeting: "Hello")
 
 
 
@@ -33,11 +38,12 @@ greeting("Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
+
 func daysInMonth(month: String, days: String) {
-    print("There are \(days) in \(month)")
+    print("There are \(days) days in \(month)")
 }
 
-daysInMonth("November", days: 30)
+daysInMonth("November", days: "30")
 
 
 
@@ -45,7 +51,12 @@ daysInMonth("November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func bottlesOnWall(number: Int, name: String, place: String){
+    print("\(number) bottles of \(name) on the \(place) wall.")
+}
+
+bottlesOnWall(16, name: "Kolsch", place: "back")
+
 
 
 
